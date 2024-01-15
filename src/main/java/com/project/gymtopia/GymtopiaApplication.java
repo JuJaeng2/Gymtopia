37e4bc4eb5119fa81f0384dabc3bffa69545604f
jujaeyeong <jjy5230@gmail.com>
2024-01-15 22:26:46 +0900

    Initial commit

---
+++ src/main/java/com/project/gymtopia/GymtopiaApplication.java
@@ -0,0 +1,13 @@
+package com.project.gymtopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtopiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymtopiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/gymtopia/GymtopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gymtopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymtopiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
