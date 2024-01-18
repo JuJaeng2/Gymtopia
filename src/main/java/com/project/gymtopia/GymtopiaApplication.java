e903b03c225b9623e24836e985cfaea62fb8ac78
jujaeyeong <jjy5230@gmail.com>
2024-01-19 01:38:26 +0900

    [Docs] README.md 수정

--- src/main/java/com/project/gymtopia/GymtopiaApplication.java
+++ src/main/java/com/project/gymtopia/GymtopiaApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GymtopiaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GymtopiaApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(GymtopiaApplication.class, args);
+  }
 
 }
+
